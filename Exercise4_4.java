485c3ebc0a1bb69c373a32a7b7ddb4f0f5f8614c
Nisanur <nisaeltekin@gmail.com>
2025-12-02 21:53:27 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,8 @@
+ * Ad Soyad: [Nisanur Eltekin]
+ * Ogrenci No: [250541100]
+ * Tarih: [02/12/2025]
+ * Aciklama: Gorev 4
+
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,9 +17,12 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
-
+        //return değerini kullanmazsak hata olmaz, sadece boşa gitmiş olur.
+        
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        //bu bir void metodu olduğundan dolayı hiçbir değer döndürmez. +7 yazarsak java hata verir 
+        çünkü + değeri toplamak ister ama println hiçbir değer döndürmediği için ortada değer yoktur.
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
