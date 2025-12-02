abaf3d8a561e3e9590bb9b861420e6078e9eba18
Nisanur <nisaeltekin@gmail.com>
2025-12-02 19:27:30 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,31 @@
-public class Exercise4_1 {
+ * Ad Soyad: [Nisanur Eltekin]
+ * Ogrenci No: [250541100]
+ * Tarih: [02/12/2025]
+ * Aciklama: Gorev 1 
 
+
+public class v1 {
+
+    // Amerikan formatı: Monday, July 22, 2019
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println("Amerikan Formatı:");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
+    // Avrupa formatı: 22 July 2019, Monday
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println("Avrupa Formatı:");
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        printAmerican(day, date, month, year);
+        printEuropean(day, date, month, year);
     }
 }
+
